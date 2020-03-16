d8c943c74dc27f72657cdfe6b799965bcde11985
sharan <sharan.reddy@insight.com>
2020-03-16 17:07:04 +0800

    child component routing

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data: Array<object> = [
+    { name: "sharan", age: 28, job: "developer" },
+    { name: "rithika", age: 28, job: "plant-doctor" },
+    { name: "buddi", age: 2, job: "just-playing" }
+  ]
+
+  headers: Array<object> = [
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "job", label: "Job" }
+  ]
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/collections/table/table.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  @Input('class') classNames = '';
+  @Input() data = [];
+  @Input() headers = [];
+  constructor() {
+    console.log(this.headers);
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
